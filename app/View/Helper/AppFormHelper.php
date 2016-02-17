a0da77e84c856100e1f0bfbf1595cab7e47e2f6e
Jamie Desktop <jamiebclark@gmail.com>
2016-02-17 01:02:55 -0500

    Created AppFormHelper and integrated with BoostCakeFormHelper

---
+++ app/View/Helper/AppFormHelper.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Extends the basic FormHelper
+ *
+ **/
+
+App::uses('BoostCakeFormHelper', 'BoostCake.View/Helper');
+App::uses('Hash', 'Utility');
+
+class AppFormHelper extends BoostCakeFormHelper {
+
+/**
+ * Extends Form create function to allow site-wise input defaults
+ *
+ * @param string $model The current model
+ * @param array $options Additional form options
+ * @return string The opening form tag
+ **/
+	public function create($model = null, $options = array()) {
+		$options = Hash::merge(array(
+			'inputDefaults' => array(
+				'div' => 'form-group',
+				'wrapInput' => false,
+				'class' => 'form-control',
+			)
+		), $options);
+		return parent::create($model, $options);
+	}
+
+	public function input($fieldName, $options = array()) {
+		$this->setEntity($fieldName);
+
+		if (!empty($options['multiple']) && empty($options['class'])) {
+			$options['class'] = $options['multiple'];
+		}
+
+		if (!empty($options['type'])) {
+			$type = $options['type'];
+		} else {
+			$checkOptions = $this->_parseOptions($options);
+			$type = $checkOptions['type'];
+		}
+
+		if ($type == 'radio') {
+			$options = $this->addClass($options, 'radio');
+		} else if ($type == 'checkbox') {
+			$options = $this->addClass($options, 'checkbox');
+		} else if ($type == 'phone') {
+			$options = $this->append('<i class="fa fa-phone"></i>', $options);
+		} else if ($type == 'email') {
+			$options = $this->append('<i class="fa fa-at"></i>', $options);
+		}
+
+		return parent::input($fieldName, $options);
+	}
+
+	public function submit($caption = null, $options = []) {
+		$options = $this->addClass($options, 'btn btn-primary btn-lg');
+		return parent::submit($caption, $options);
+	}
+
+	/*
+	public function inputs($fields = null, $blacklist = null, $options = []) {
+		if (!empty($fields['legend'])) {
+			$legend = $fields['legend'];
+		}
+		$fields['fieldset'] = false;
+		$fields['legend'] = false;
+		$out = parent::inputs($fields, $blacklist, $options);
+
+		if (!empty($legend)) {
+			$out = $this->Html->div('panel-body', $out);
+			$out = $this->Html->div('panel panel-default', $this->Html->div('panel-heading', $legend) . $out);
+		}
+		return $out;
+	}
+	*/
+
+	public function prepend($content, $options = array()) {
+		$options['beforeInput'] = '<div class="input-group"><span class="input-group-addon">' . $content . '</span>';
+		$options['afterInput'] = '</div>';
+		return $options;
+	}
+
+	public function append($content, $options = array()) {
+		$options['beforeInput'] = '<div class="input-group">';
+		$options['afterInput'] = '<span class="input-group-addon">' . $content . '</span></div>';
+		return $options;
+	}
+
+}
