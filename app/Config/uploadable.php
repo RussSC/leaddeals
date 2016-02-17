9c1bf25f355d44d822947f706cec3ea8b5cd7d12
Jamie Desktop <jamiebclark@gmail.com>
2016-02-17 01:06:30 -0500

    Updated Uploadable image size settings

---
+++ app/Config/uploadable.php
@@ -0,0 +1,15 @@
+<?php
+$config['Uploadable'] = [
+	// Default thumbnail sizes
+	'sizes' => [
+		'full' => [],
+		'thumbnail' => ['set' => [80,80]],
+		'thumbnail-sm' => ['set' => [40, 40]],
+		'thumbnail-md' => ['set' => [240, 240]],
+		'thumbnail-lg' => ['set' => [360, 360]],
+		'banner' => ['set' => [1200,400]],
+		'small' => ['setSoft' => [80,80]],
+		'mid' => ['max' => [120,240]],
+		'tiny' => ['set' => [20,20]]
+	]
+];
