0e7b53d3afba41186f73acc1322084c4ae6c72c6
Jamie Desktop <jamiebclark@gmail.com>
2016-03-06 14:57:04 -0500

    Fixed form date input

--- app/webroot/js/script.js
+++ app/webroot/js/script.js
@@ -22,7 +22,7 @@
 (function($) {
 	function zeroPad(n) {
 		if (n < 0) {
-			n = "0" + n;
+			n = "0" + String(n);
 		}
 		return n;
 	}
@@ -30,25 +30,39 @@
 	$.fn.inputDate = function() {
 		return this.each(function() {
 			var $this = $(this),
-				$month = $(':input[name*="[month]"]', $this).hide(),
-				$day = $(':input[name*="[day]"]', $this).hide(),
-				$year = $(':input[name*="[year]"]', $this).hide(),
-				$hour = $(':input[name*="[hour]"]', $this).hide(),
-				$min = $(':input[name*="[min]"]', $this).hide(),
-				$meridian = $(':input[name*="[meridian]"]', $this).hide(),
-				$datepicker = $('<input/>', {
+				$formVals = $('<div>').appendTo($this),
+				$inputs = {},
+				dateKeys = ['month', 'day', 'year', 'hour', 'min', 'meridian'];
+
+			for(var i in dateKeys) {
+				var key = dateKeys[i],
+					find = ':input[name*="[' + key + ']"]';
+				$(find, $this).detach().appendTo($formVals);
+				$inputs[key] = $(find, $formVals);
+			}
+
+			var $datepicker = $('<input/>', {
 					name: 'test_name',
 					type: 'text',
 					'class': 'form-control input-date-date'
 				}).appendTo($this);
+
+			$formVals.css({
+				position: 'absolute',
+				opacity: 0,
+				top: '-99999px',
+				left: '-99999px',
+			});
+			$(':input', $formVals).attr('tabIndex', -1).prop('required', false);
+
 			$datepicker
 				.datepicker()
 				.change(function() {
 					var date = $(this).datepicker("getDate"),
 						D = new Date(date),
 						m = D.getMonth() + 1,
-						d = D.getDay() + 1,
-						y = D.getYear(),
+						d = D.getDate(),
+						y = D.getFullYear(),
 						H = D.getHours(),
 						i = D.getMinutes(),
 						A = 'am';
@@ -60,16 +74,27 @@
 					} else if (H == 0) {
 						H = 12;
 					}
-					$month.val(zeroPad(m));
-					$day.val(zeroPad(d));
-					$year.val(zeroPad(y));
-					if ($hour.length) {
-						$hour.val(zeroPad(H));
-						$min.val(zeroPad(i));
-						$meridian.val(A);
+
+					var updateVals = {
+						'month': 	zeroPad(m),
+						'day': 		zeroPad(d),
+						'year': 	y,
+					};
+
+					for (var i in updateVals) {
+						var v = updateVals[i];
+						$('option', $inputs[i]).each(function() {
+							$(this).prop('selected', $(this).attr('value') == v);
+						});
+					}
+		
+					if ($inputs.hour.length) {
+						$inputs.hour.val(zeroPad(H));
+						$inputs.min.val(zeroPad(i));
+						$inputs.meridian.val(A);
 					}
 				})
-				.datepicker('setDate', $month.val() + "/" + $day.val() + "/" + $year.val());
+				.datepicker('setDate', $inputs.month.val() + "/" + $inputs.day.val() + "/" + $inputs.year.val());
 		});
 	};
 
